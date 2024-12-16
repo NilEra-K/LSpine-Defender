b72fc10a0518c6fcdb3418ecdf934bd756f81d1f
NilEra-K <1141778284@qq.com>
2024-12-16 11:02:01 +0800

    后端：添加测试接口

--- workstation/backend/api/predict-api.py
+++ workstation/backend/api/predict-api.py
@@ -6,6 +6,15 @@ app = FastAPI()
 async def read_test():
     return {"message": "This is a test endpoint"}
 
+@app.get("/api/v1/one-stage-predict")
+async def oneStagePredict(image):
+    return {"message": "/api/v1/one-stage-predict 测试接口"}
+
+@app.get("/api/v1/multi-models-predict")
+async def read_test():
+    return {"message": "/api/v1/multi-models-predict 测试接口"}
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=16022)
